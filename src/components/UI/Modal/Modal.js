533e1fc785a518b788fb1944c34ce0f868cc980d
Roger B <myvarequals@gmail.com>
2019-02-20 19:56:12 -0500

    modal functioning properly

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,15 +3,16 @@ import Overlay from '../Overlay/Overlay'
 import { FontAwesomeIcon } from '../../../../node_modules/@fortawesome/react-fontawesome';
 import classes from './Modal.module.css';
 
-const modal = props => (
-    <>
-        <Overlay />
-        <div className={classes.ModalBox}>        
-            <p>Confirm removal</p>
-            <FontAwesomeIcon icon="check-circle" onClick={props.confirm} />
-            <FontAwesomeIcon icon="times-circle" onClick={props.cancel} />
-        </div>
-    </>
+const modal = props => ( 
+        <>
+            <div className={classes.ModalBox}>        
+                <p>Are you sure?</p>
+                <FontAwesomeIcon icon="check-circle" onClick={props.confirm} className={classes.CheckCircle} />
+                <FontAwesomeIcon icon="times-circle" onClick={props.cancel} className={classes.TimesCircle} />            
+                
+            </div>
+            <Overlay />
+        </>
 );
 
 
